68f1904dfc85b6317cc950c558b11daea5c5b7b1
DomiDelarosa <marengodelarosa2@gmail.com>
2026-05-06 12:35:43 -0500

    clase main

--- Program.cs
+++ Program.cs
@@ -1,7 +1,13 @@
-﻿public class Program 
+﻿public class Program
 {
-    static void Main()
+    static void Main(string[] args)
     {
-        Console.WriteLine("Hola, mundo"); 
+        Console.WriteLine("Empleado 1:");
+        Empleado empleado1 = new Empleado("Juan", 30, "12345678", "juan@example.com", "Entrenador", 0);
+        empleado1.MostrarInfo();
+
+        Console.WriteLine("Cliente 1:");
+        Cliente cliente1 = new Cliente("Maria", 25, "87654321", "maria@example.com", "Plan A", "2023-01-01");
+        cliente1.MostrarInfo();
     }
 }
