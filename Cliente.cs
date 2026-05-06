b19d8ebc1f4f5422e1221b58042e2b51cb2d1f70
DomiDelarosa <marengodelarosa2@gmail.com>
2026-05-06 12:28:42 -0500

    fixing Cliente.cs

--- Cliente.cs
+++ Cliente.cs
@@ -4,15 +4,15 @@ public class Cliente : Persona
       public string fechaIngreso { get; }
       public bool activo { get; set; }
       public Cliente(string nombre, int edad, string documento, string correo, string plan, string fecha)
-         : base(nombre, documento, edad, correo)
+         : base(nombre, documento, correo, edad)
       {
          this.plan = plan;
          this.fechaIngreso = fecha;
          this.activo = true; 
       }
-      public override void MostrarInformacion()
+      public override void MostrarInfo()
       {
-         base.MostrarInformacion();
+         base.MostrarInfo();
          Console.WriteLine(plan);
       }
 }
